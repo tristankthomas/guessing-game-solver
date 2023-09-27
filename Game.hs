7ca053748c4cc86dfb79e56400d335c19caadcee
tristankthomas <tristankthomas@icloud.com>
2023-09-27 22:37:50 +1000

    Initial files created

---
+++ Game.hs
@@ -0,0 +1,8 @@
+-- Game.hs: defines the functions for the guessing game
+-- Author : Tristan Thomas
+-- Date   : 27-9-23
+
+module Game (Location, toLocation, fromLocation, feedback,
+              GameState, initialGuess, nextGuess) where
+
+                
---
+++ Main.hs
@@ -0,0 +1,50 @@
+--  File     : Main.hs
+--  Author   : Peter Schachte
+--  Purpose  : Test program for guessing game project to be used in Grok
+
+
+module Main where
+
+import System.Exit
+import Game (Location, toLocation, fromLocation, feedback,
+              GameState, initialGuess, nextGuess)
+
+testCase = "F1 D2 G4"
+
+-- | Main code to test guessing game implementation.
+main :: IO ()
+main = do
+    case mapM toLocation $ words testCase of
+        Just target@[_,_,_] ->
+            proj2test target
+        _ -> do
+            putStrLn $ "toLocation Failed to convert one of " ++ testCase
+                 ++ " to a Location"
+            exitFailure
+
+
+-- | Guess the given target, counting and showing the guesses.
+gametest :: [Location] -> IO ()
+gametest target = do
+    putStrLn $ "Searching for target " ++ showLocations target
+    let (guess,other) = initialGuess
+    loop target guess other 1
+
+
+-- | Given a target and guess and a guess number, continue guessing
+-- until the right target is guessed.
+loop :: [Location] -> [Location] -> Game.GameState -> Int -> IO ()
+loop target guess other guesses = do
+    putStrLn $ "Your guess #" ++ show guesses ++ ":  " ++ showLocations guess
+    let answer = feedback target guess
+    putStrLn $ "    My answer:  " ++ show answer
+    if answer == (3,0,0)
+        then do
+            putStrLn $ "You got it in " ++ show guesses ++ " guesses!"
+        else do
+            let (guess',other') = nextGuess (guess,other) answer
+            loop target guess' other' (guesses+1)
+
+
+showLocations :: [Location] -> String
+showLocations = unwords . (fromLocation <$>)
