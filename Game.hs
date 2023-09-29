cb1f95bdc5469bdf39c44464e4d4ff5967d4d30e
tristankthomas <tristankthomas@icloud.com>
2023-09-29 14:26:18 +1000

    Implemented location conversion

--- Game.hs
+++ Game.hs
@@ -5,4 +5,10 @@
 module Game (Location, toLocation, fromLocation, feedback,
               GameState, initialGuess, nextGuess) where
 
-                
+type Location = (Char, Int)
+type GameState = ()
+toLocation :: String -> Maybe Location
+fromLocation :: Location -> String
+feedback :: [Location] -> [Location] -> (Int,Int,Int)
+initialGuess :: ([Location], GameState)
+nextGuess :: ([Location], GameState) -> (Int,Int,Int) -> ([Location], GameState)
